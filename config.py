03c02f75211125c972e955091920bece301c6c0d
Millyn <9047230@qq.com>
2016-10-08 16:17:19 +0800

    config

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 DEBUG = True
 SECRET_KEY = 'this is key'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysqlpass@localhost:3306/rzcy'
-# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://id:password@localhost:3306/database'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://id:password@localhost:3306/database'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
